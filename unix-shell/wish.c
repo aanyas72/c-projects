ebd298d496f7c9b85b3306ddd3dd0f45cc8bb9b0
aanyasingh <aanyasingh604@gmail.com>
2025-07-23 14:23:10 -0500

    break up input

--- unix-shell/wish.c
+++ unix-shell/wish.c
@@ -1,28 +1,45 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 void interactive_mode() {
     while (1) {
         printf("%s", "wish> ");
         
         char *command = NULL;
+        char *exit_command = "exit";
         size_t len = 0;
         ssize_t state;
         
         state = getline(&command, &len, stdin);
 
-        if (state != -1) {
-            // handle command and free it
-            free(command);
-        } else {
-            if (feof(stdin)) { // exit gracefully
+        if (state != -1) { // state is valid
+            if (command[state - 1] == '\n') {
+                command[state - 1] = '\0';
+            }
+            
+            if (strcmp(command, exit_command) == 0) {
                 free(command);
+                exit(0);
+            } else { // handle command and free it
+
+                const char *delim = " ";
+                // char *command_toks = strsep(&command, delim);
+                char *command_toks;
+                while ((command_toks = strsep(&command, delim)) != NULL) {
+                    printf("%s\n", command_toks);
+                }
+                free(command);
+            }
+        } else { // state is invalid
+            free(command);
+            
+            if (feof(stdin)) { // exit gracefully
+                printf("Exiting\n");
                 exit(0);
             } else { // handle error
                 printf("An error has occurred.\n");
-                free(command);
-                exit(-1);
+                exit(1);
             }
         }
     }
